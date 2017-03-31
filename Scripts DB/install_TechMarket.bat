4a9473487fedf7032344bd36e5ec568c0d5e55fc
jonathanCapitao <jonathan.capitao.12587@gmail.com>
2017-03-31 12:23:04 +0200

    nétoyage log db

--- Scripts DB/install_TechMarket.bat
+++
@@ -1,9 +0,0 @@
-REM Fichier		: install_TechMarket.bat
-REM Objet		: Création de la BDD TechMarket
-REM Author		: CAPITAO Jonathan
-
-mkdir .\Logs
-
-REM Lancement du script 01_BDD_TechMarket.sql dans SQL*PLUS
-REM le %1 permet de demander le mot de passe lors du lancement du script
-sqlplus SYSTEM/manager@HEGLOCAL @01_BDD_TechMarket.sql
--- Scripts DB/uninstall_TechMarket.bat
+++
@@ -1,9 +0,0 @@
-REM Fichier		: uninstall_TechMarket.bat
-REM Objet		: Désinstallation de la BDD TechMarket 
-REM Author		: CAPITAO Jonathan
-
-mkdir .\Logs
-
-REM Lancement du script uninstall_TechMarket.sql dans SQL*PLUS
-REM le %1 permet de demander le mot de passe lors du lancement du script
-sqlplus SYSTEM/manager@HEGLOCAL @10_uninstall_TechMarket.sql
